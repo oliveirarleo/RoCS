e485675046c02de9f20ac7570bb6edb1e5c361c9
Leonardo O Ramos <leo.o.rms@gmail.com>
2018-04-28 15:22:48 -0300

    intermediate state from architecture decisions

---
+++ src/Pioneer_p3dx/Monitor/sonar_vrep_publisher.cpp
@@ -0,0 +1,7 @@
+//
+// Laboratory of Robotics and Cognitive Science
+// Created by:  Leonardo de Oliveira Ramos
+// Github:      https://github.com/oramleo
+//
+
+#include "SonarVREPPublisher.h"
---
+++ src/Pioneer_p3dx/Monitor/sonar_vrep_publisher.h
@@ -0,0 +1,18 @@
+//
+// Laboratory of Robotics and Cognitive Science
+// Created by:  Leonardo de Oliveira Ramos
+// Github:      https://github.com/oramleo
+//
+
+#ifndef ROCS_SONARVREPPUBLISHER_H_
+#define ROCS_SONARVREPPUBLISHER_H_
+
+#include "Monitor/monitor_publisher.h"
+
+class SonarVREPPublisher: public MonitorPublisher
+{
+
+};
+
+
+#endif // ROCS_SONARVREPPUBLISHER_H_
