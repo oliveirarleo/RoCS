afcdda805d65312d1fe2f6aa3c86d3e7e7f59771
oramleo <hardship6arrow>
2018-12-07 21:51:26 +0100

    deletando astar

--- pioneer_astar/PioneerP3DX/Execute/Actions/turn_right.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by leonardo on 04/10/18.
-//
-
-#include "turn_right.h"
--- pioneer_astar/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "PioneerP3DX/OriginPioneer/origin_pioneer.h"
-
-
-int main()
-{
-	OriginPioneer a_star_pioneer{};
-
-	return 0;
-}
